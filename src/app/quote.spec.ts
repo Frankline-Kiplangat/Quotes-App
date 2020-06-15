4b471b71b9d1ac6fca1fde72e5cf4be12fddc9cc
Frank <kipfrankline@gmail.com>
2020-06-15 05:39:17 +0300

    Deploy to gh-pages

--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -1,8 +1,7 @@
 import { Quote } from './quote';
 
-const newLocal = new Date();
 describe('Quote', () => {
   it('should create an instance', () => {
-    expect(new Date).toBeTruthy();
+    expect(new Quote()).toBeTruthy();
   });
 });
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,3 +4,12 @@ export class Quote {
       this.showDetails=false;
     }
   }
+//   public like:number
+//   public dislike:number
+//   public completeDate:Date
+//   constructor(public name:string, public author:string,public quote:string){
+//     this.like =0;
+//     this.dislike =0;
+//     this.completeDate=new Date;
+//   }
+// }
